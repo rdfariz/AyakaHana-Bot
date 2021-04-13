236e71da2bda4e59503d2b1978dd1e8587938259
rdfariz <rfarizinsanp@gmail.com>
2021-04-14 01:18:10 +0700

    upd general

--- modules/general.js
+++ modules/general.js
@@ -11,27 +11,27 @@ module.exports = async (client, msg) => {
   const idFandy = "806402900766490644"
 
   const author = msg.author.id
-  if (author !== idIcal && (query === 'cal' || query.includes('ical'))) {
+  if (author !== idIcal && (query === 'cal' || query.includes(' cal') || query.includes('cal ') || || query.includes('ical'))) {
     msg.channel.send(`<@${idIcal}> dipanggil <@${author}> tuh`)
   }
-  if (author !== idArip && (query === 'rip' || query === 'rif' || query.includes('rip') || query.includes('rif') || query.includes('arip') || query.includes('arief') || query.includes('aray'))) {
+  if (author !== idArip && (query === 'rip' || query === 'rif' || query.includes(' rip') || query.includes('rip ') || query.includes('rif') || query.includes('arip') || query.includes('arief') || query.includes('aray'))) {
     msg.channel.send(`<@${idArip}> dipanggil <@${author}> tuh`)
   }
-  if (author !== idAkbar && (query === 'bar' || query.includes('akbar'))) {
+  if (author !== idAkbar && (query === 'bar' || query.includes(' bar') || query.includes('bar ') || query.includes('akbar'))) {
     msg.channel.send(`<@${idAkbar}> dipanggil <@${author}> tuh`)
   }
   if (author !== idYuuqi && !query.includes('!qi') && (query === 'ki' || query === 'qi' || query.includes('yuuqi'))) {
     msg.channel.send(`<@${idYuuqi}> dipanggil <@${author}> tuh`)
   }
-  if (author !== idRey && query.includes('rey')) {
+  if (author !== idRey && (query === 'rey' || query.includes(' rey') || query.includes('rey '))) {
     msg.channel.send(`<@${idRey}> dipanggil <@${author}> tuh`)
   }
-  if (author !== idFandy && (query === 'fan' || query.includes('fandy') || query.includes('fandi'))) {
+  if (author !== idFandy && (query === 'fan' || query.includes(' fan') || query.includes('fan ') || query.includes('fandy') || query.includes('fandi'))) {
     msg.channel.send(`<@${idFandy}> dipanggil <@${author}> tuh`)
   }
 
   // Filter
-  if (query.includes('anjing') || query.includes('ajing') || query.includes('ajg')) {
+  if (query.includes('anjing') || query.includes('ajing') || query.includes('ajg') || query.includes('anj')) {
     const msgReply = ["Astaghfirullah, jgn kasar :("]
     msg.reply(msgReply[Math.floor(Math.random() * msgReply.length)])
   }
